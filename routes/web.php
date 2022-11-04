5ed2d909108d3d8f6eb5848ef5b5e281b15e263c
Jose Torres Flores <115846091+TFstorm22@users.noreply.github.com>
2022-11-04 16:19:22 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Barra de Navegacion</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio <span class="sr-only">(Actual)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Caracteristicass</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Precios</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+    Texto de la barra de navegación con un elemento en línea
+    </span>
+  </div>
+</nav>
