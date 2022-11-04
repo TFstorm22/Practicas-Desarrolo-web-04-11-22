c88eea04197d2e9bf1777d626deb763ee6cd66fa
Jose Torres Flores <115846091+TFstorm22@users.noreply.github.com>
2022-11-04 18:52:26 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,10 @@
 						<p>Curiosidades &nbsp;&bull;&nbsp; Comidas Tipicas &nbsp;&bull;&nbsp; Inicio</p>
 						<nav>
 							<ul>
-								<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-								<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-								<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-								<li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+								<li><a href="https://www.machupicchu.gob.pe/contacto/" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="https://www.machupicchu.gob.pe/contacto/" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+								<li><a href="https://dribbble.com/tags/machu_picchu" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+								<li><a href="https://www.machupicchu.gob.pe/contacto/" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
                                 <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
                                 <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.machupicchu.gob.pe/historia/" class="underline text-gray-900 dark:text-white">Historia Machupichu</a></div>
