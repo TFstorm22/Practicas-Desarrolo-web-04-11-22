6cfb6700a9d5b01cebe1abbb5ffd42c89bb9f1b6
TFstorm22 <1376280@senati.pe>
2022-11-11 03:55:03 -0500

    Lugares turisticos

--- routes/web.php
+++ routes/web.php
@@ -5,48 +5,173 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html>
-	<head>
-		<title>Aerial by HTML5 UP</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-	</head>
-	<body class="is-preload">
-		<div id="wrapper">
-			<div id="bg"></div>
-			<div id="overlay"></div>
-			<div id="main">
 
-				<!-- Header -->
-					<header id="header">
-						<h1>Jose Angel avance</h1>
-						<p>Curiosidades &nbsp;&bull;&nbsp; Comidas Tipicas &nbsp;&bull;&nbsp; Inicio</p>
-						<nav>
-							<ul>
-								<li><a href="https://www.machupicchu.gob.pe/contacto/" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-								<li><a href="https://www.machupicchu.gob.pe/contacto/" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-								<li><a href="https://dribbble.com/tags/machu_picchu" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-								<li><a href="https://www.machupicchu.gob.pe/contacto/" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
-                                <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://www.machupicchu.gob.pe/historia/" class="underline text-gray-900 dark:text-white">Historia Machupichu</a></div>
-                            </div>
+<head>
+	<title>Lugares Turisticos de Huanuco</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<noscript>
+		<link rel="stylesheet" href="assets/css/noscript.css" />
+	</noscript>
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,700" rel="stylesheet">
+
+	<link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+	<link rel="stylesheet" href="{{ asset('fonts/ionicons/css/ionicons.min.css') }}">
+
+	<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+
+	<link rel="stylesheet" href="{{ asset('fonts/flaticon/font/flaticon.css') }}">
+
+	<link rel="stylesheet" href="{{ asset('fonts/fontawesome/css/font-awesome.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/select2.css') }}">
+
+
+	<link rel="stylesheet" href="{{ asset('css/helpers.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body class="is-preload">
+	<div id="wrapper">
+		<div id="bg"></div>
+		<div id="overlay"></div>
+		<div id="main">
+
+			<section class="probootstrap_section bg-light">
+				<div class="container">
+					<div class="row text-center mb-5 probootstrap-animate">
+						<div class="col-md-12">
+							<h2 class="display-4 border-bottom probootstrap-section-heading">Lugares Turisticos</h2>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6">
+
+							<div class="media probootstrap-media d-flex align-items-stretch mb-4 probootstrap-animate">
+								<div class="probootstrap-media-image" style="background-image: url({{asset('img/Machu_Picchu.jpg')}})">
+								</div>
+								<div class="media-body">
+									<h5 class="mb-3">Machu Picchu</h5>
+									<p>Machu Picchu es una de las 7 maravillas del mundo moderno. Se trata de uno de los lugares preferidos de los viajeros en el mundo. Y no solo por la increíble ciudad de piedra erigida por los incas, sino por todo lo que rodea a este gran destino turístico. La experiencia de viajar a la histórica ciudad inca es única en el mundo. Sus paisajes, sus construcciones, historias, todo hace que, definitivamente, Machu Picchu sea un destino imperdible.</p>
+									
+								</div>
+							</div>
+
+							<div class="media probootstrap-media d-flex align-items-stretch mb-4 probootstrap-animate">
+								<div class="probootstrap-media-image" style="background-image: url(../resources/images/img_2.jpg)">
+								</div>
+								<div class="media-body">
+									<h5 class="mb-3">Las Islas Ballestas</h5>
+									<p>Con un halo de misterio, el Candelabro da la bienvenida a los visitantes que se dirigen a las Islas Ballestas. Este geoglifotiene una extensión aproximada de 170 metros.
+
+										Las islas Ballestas son parte de la Reserva Nacional de Sistema de Islas, Islotes y Puntas Guaneras. En sus alrededores viven una gran colonia de lobos marinos a los que se pueden observar fácilmente desde una lancha a motor. Este hábitat es compartido con los pingüinos de Humboldt y piqueros.
+
+										La magia de las Islas Ballestas está en su naturaleza. La travesía a las islas dura aproximadamente dos horas y se inicia en la mañana.</p>
+								</div>
+							</div>
+
+						</div>
+						<div class="col-md-6">
+
+							<div class="media probootstrap-media d-flex align-items-stretch mb-4 probootstrap-animate">
+								<div class="probootstrap-media-image" style="background-image: url(../resources/images/img_4.jpg)">
+								</div>
+								<div class="media-body">
+									<h5 class="mb-3">Fortaleza De Kuélap</h5>
+									<p>Kuélap está ubicada en la provincia de Luya, una de las siete que conforman la región de Amazonas. Los Andes nororientales fueron la locación elegida por los integrantes de la cultura Chachapoyas, los responsables de la construcción de esta enigmática ciudad, que cuenta con espacios administrativos, religiosos, ceremoniales e incluso viviendas. Se podría afirmar que era una urbe, ubicada estratégicamente y custodiada por los grandes muros, pero a su vez rodeada de una tupida naturaleza y hermosos paisajes, dignos de fotografías. </p>
+								</div>
+							</div>
+
+							<div class="media probootstrap-media d-flex align-items-stretch mb-4 probootstrap-animate">
+								<div class="probootstrap-media-image" style="background-image: url(../resources/images/img_5.jpg)">
+								</div>
+								<div class="media-body">
+									<h5 class="mb-3">Bosque De Piedras Huayllay</h5>
+									<p> El Bosque de piedras de Huayllay está ubicado en el distrito de Huayllay, provincia y departamento de Pasco, Perú tiene una extensión de 6000 ha.1​ Está protegido desde 1974 por el establecimiento del Santuario nacional de Huayllay. Constituye una maravilla natural andina, a 4,310 metros sobre el nivel del mar.
+
+										Es un atractivo turístico porque reúne más de 4.000 formaciones rocosas que semejan gigantescos perfiles humanos, como el caminante o pensador, y animales, como la tortuga, la alpaca, el cóndor, el elefante, la cebra, los pingüinos, el perro, el caracol, entre otros. </p>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- END section -->
+
+			<footer class="probootstrap_section probootstrap-border-top">
+				<div class="container">
+					<div class="row mb-5">
+						<div class="col-md-3">
+							<h3 class="probootstrap_font-18 mb-3">Quick Links</h3>
+							<ul class="list-unstyled">
+								<li><a href="https://free-template.co" target="_blank">Home</a></li>
+								<li><a href="https://free-template.co" target="_blank">About</a></li>
+								<li><a href="https://free-template.co" target="_blank">Services</a></li>
+								<li><a href="https://free-template.co" target="_blank">Contact</a></li>
 							</ul>
-						</nav>
-					</header>
+						</div>
+						<div class="col-md-3">
+							<h3 class="probootstrap_font-18 mb-3">Quick Links</h3>
+							<ul class="list-unstyled">
+								<li><a href="https://free-template.co" target="_blank">Home</a></li>
+								<li><a href="https://free-template.co" target="_blank">About</a></li>
+								<li><a href="https://free-template.co" target="_blank">Services</a></li>
+								<li><a href="https://free-template.co" target="_blank">Contact</a></li>
+							</ul>
+						</div>
+						<div class="col-md-3">
+							<h3 class="probootstrap_font-18 mb-3">Quick Links</h3>
+							<ul class="list-unstyled">
+								<li><a href="https://free-template.co" target="_blank">Home</a></li>
+								<li><a href="https://free-template.co" target="_blank">About</a></li>
+								<li><a href="https://free-template.co" target="_blank">Services</a></li>
+								<li><a href="https://free-template.co" target="_blank">Contact</a></li>
+							</ul>
+						</div>
+						<div class="col-md-3">
+							<h3 class="probootstrap_font-18 mb-3">Quick Links</h3>
+							<ul class="list-unstyled">
+								<li><a href="https://free-template.co" target="_blank">Home</a></li>
+								<li><a href="https://free-template.co" target="_blank">About</a></li>
+								<li><a href="https://free-template.co" target="_blank">Services</a></li>
+								<li><a href="https://free-template.co" target="_blank">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
 
-				<!-- Footer -->
-					<footer id="footer">
-						<span class="copyright">&copy; TFstorm22: <a href="http://html5up.net">HTML</a>.</span>
-					</footer>
 
-			</div>
 		</div>
-		<script>
-			window.onload = function() { document.body.classList.remove('is-preload'); }
-			window.ontouchmove = function() { return false; }
-			window.onorientationchange = function() { document.body.scrollTop = 0; }
-		</script>
-	</body>
+	</div>
+
+
+	<script src="{{ asset('js/jquery.min.js') }}"></script>
+	<script src="{{ asset('js/popper.min.js') }}"></script>
+	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+
+	<script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>
+	<script src="{{ asset('js/jquery.waypoints.min.js') }}"></script>
+	<script src="{{ asset('js/jquery.easing.1.3.js') }}"></script>
+
+	<script src="{{ asset('js/select2.min.js') }}"></script>
+
+	<script src="{{ asset('js/main.js') }}"></script>
+	<script>
+		window.onload = function() {
+			document.body.classList.remove('is-preload');
+		}
+		window.ontouchmove = function() {
+			return false;
+		}
+		window.onorientationchange = function() {
+			document.body.scrollTop = 0;
+		}
+	</script>
+</body>
+
 </html>
